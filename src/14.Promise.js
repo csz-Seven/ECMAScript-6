8f82fef50afbc3f9ced1946b2e9f9d77cb2b286c
Lai.gongqi <it.laigq@gmail.com>
2018-07-19 15:33:07 +0800

    书签

--- src/14.Promise.js
+++ src/14.Promise.js
@@ -119,6 +119,7 @@
 // ★★★  实例6  ★★★
 // Promise.prototype.catch()
 // promise抛出一个错误，就被catch方法指定的回调函数捕获
+// reject方法的作用，等同于抛出错误
 {
     // 写法1
     const promise1 = new Promise(function (resolve, reject) {
@@ -148,3 +149,10 @@
         console.log(error)
     })
 }
+
+
+/*
+* 书签
+* 2018-07-19 15:32:14
+* 上面代码中，someAsyncThing函数产生的 Promise 对象，内部有语法错误。浏览器运行到这一行，会打印出错误提示ReferenceError: x is not defined，但是不会退出进程、终止脚本执行，2 秒之后还是会输出123。这就是说，Promise 内部的错误不会影响到 Promise 外部的代码，通俗的说法就是“Promise 会吃掉错误”。
+* */
