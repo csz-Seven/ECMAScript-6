6710798aff9f9ca5398aeac063cd1608fb94ddc8
Lai.gongqi <it.laigq@gmail.com>
2018-07-20 16:31:09 +0800

    实例11

--- src/14.Promise.js
+++ src/14.Promise.js
@@ -156,3 +156,107 @@
 * 2018-07-19 15:32:14
 * 上面代码中，someAsyncThing函数产生的 Promise 对象，内部有语法错误。浏览器运行到这一行，会打印出错误提示ReferenceError: x is not defined，但是不会退出进程、终止脚本执行，2 秒之后还是会输出123。这就是说，Promise 内部的错误不会影响到 Promise 外部的代码，通俗的说法就是“Promise 会吃掉错误”。
 * */
+
+
+// ★★★  实例7  ★★★
+// Promise.prototype.catch()
+// catch接收内部抛出的错误
+{
+    const someAsyncThing = function () {
+        return new Promise(function (resolve, reject) {
+            resolve(x + 2);
+        })
+    }
+
+    someAsyncThing()
+        .catch(function (err) {
+            console.log('oh no', err)
+        })
+        .then(function () {
+            console.log('实例7')
+        })
+}
+
+
+// ★★★  实例8  ★★★
+// Promise.prototype.catch()
+// catch内部可以在抛出错误，如需要接收 需要catch().catch()
+{
+    const someAsyncThing = function () {
+        return new Promise(function (resolve, reject) {
+            resolve(x + 2);
+        })
+    }
+    someAsyncThing().then(function () {
+    }).catch(function (err) {
+        console.log('实例8', err)
+        y + 2;
+    }).catch(function (err) {
+        console.log('实例8', err)
+    })
+}
+
+
+// ★★★  实例9  ★★★
+// Promise.prototype.finally()
+// finally方法用于指定不管 Promise 对象最后状态如何，都会执行的操作.
+// finally本质上是then方法的特例.
+{
+    // promise
+    //     .then(result => {})
+    //     .catch(error => {})
+    //     .finally(() => {});
+
+    // 等价于↓
+    // promise
+    //     .finally(()=>{
+    //
+    //     })
+
+    // then实现方法
+    // promise
+    //     .then(
+    //         result => {
+    //             return result;
+    //         },
+    //         error => {
+    //             throw error;
+    //         }
+    //     )
+}
+
+
+// ★★★  实例10  ★★★
+// Promise.all()
+// 用于将多个 Promise 实例，包装成一个新的 Promise 实例.
+{
+    // const p = Promise.all([p1, p2, p3]);
+    // （1）只有p1、p2、p3的状态都变成fulfilled，p的状态才会变成fulfilled，此时p1、p2、p3的返回值组成一个数组，传递给p的回调函数。
+
+    // （2）只要p1、p2、p3之中有一个被rejected，p的状态就变成rejected，此时第一个被reject的实例的返回值，会传递给p的回调函数。
+
+    //  如果作为参数的 Promise 实例，自己定义了catch方法，那么它一旦被rejected，并不会触发Promise.all()的catch方法。
+}
+
+
+// ★★★  实例11  ★★★
+// Promise.all()
+{
+    const p1 = new Promise((resolve, reject) => {
+        resolve('实例11-p1')
+    })
+        .then(result => {
+            result
+        })
+        .catch(e => e);
+
+    const p2 = new Promise((resolve, reject) => {
+        throw new Error('实例11-p2-报错');
+    })
+        .then(result => result)
+        .catch(e => e)
+
+    Promise.all([p1,p2])
+        .then(result => console.log(result))
+        .catch(e => console.log(e))
+}
