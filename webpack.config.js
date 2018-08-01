6e203519186b1aa14640de9e077957d3aea10d91
Lai.gongqi <it.laigq@gmail.com>
2018-08-01 11:32:09 +0800

    引入webpack

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path')
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        filename: "bundle.js",
+        path:path.resolve(__dirname,'dist')
+    }
+}
+/**
+ *  作者:Seven
+ *  时间:2018/8/1 11:29
+ *  Email:csz.seven@gmail.com
+ *  描述:记录
+ *  webpack指南：
+ *      1.指南
+ *      2.安装
+ *      3.起步
+*/
