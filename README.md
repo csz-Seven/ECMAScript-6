7bfb6843b246f714b8239142c3058ffc83062c26
Lai.gongqi <it.laigq@gmail.com>
2018-08-02 15:28:48 +0800

    16.Generator-与Iterator的关系

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 ECMAScript 6
-命令：npx babel src/XXX.js --watch --out-file dist/index.js
+命令：npx run start
--- src/16.Generator.js
+++ src/16.Generator.js
@@ -33,8 +33,102 @@
 
     let hw = helloWorldGenerator();
 
-    hw.next()
-    hw.next()
-    hw.next()
-
+    console.log('yield1', hw.next())
+    console.log('yield2', hw.next())
+    console.log('yield3', hw.next())
+    console.log('yield4', hw.next())
 }
+
+
+/**
+ *  作者:Seven
+ *  时间:2018/8/2 14:49
+ *  Email:csz.seven@gmail.com
+ *  描述:实例2-yield
+ */
+{
+    // yield惰性求值,只有调用next方法才开始执行
+    // *位置没有准确的标准
+
+    // yield与return的区别:
+    //  1.yield可以暂停运行，位置记录功能。return不具有位置记忆功能。
+    //  2.function执行次数: yield多次 return单次
+    function* f() {
+        yield 7 + 7
+    }
+
+    // yield不能使用在普通函数
+}
+
+
+/**
+ *  作者:Seven
+ *  时间:2018/8/2 14:58
+ *  Email:csz.seven@gmail.com
+ *  描述:实例3-Generator函数不使用yield函数，单纯的暂停函数
+ */
+{
+    function* f() {
+        console.log('实例3-Generator暂停函数')
+    }
+
+    var generator = f();
+
+    setTimeout(function () {
+        generator.next()
+    }, 2000)
+}
+
+
+/**
+ *  作者:Seven
+ *  时间:2018/8/2 15:10
+ *  Email:csz.seven@gmail.com
+ *  描述:实例4-yield表达式如果用在另一个表达式之中，必须放在圆括号里面
+ */
+{
+    function* demo() {
+        console.log('实例4 ' + (yield));
+        console.log('实例4 ' + (yield 123))
+    }
+
+    let generator = demo()
+}
+
+
+/**
+ *  作者:Seven
+ *  时间:2018/8/2 15:15
+ *  Email:csz.seven@gmail.com
+ *  描述:实例5-Iterator接口关系
+ */
+{
+    //Generator 函数就是遍历器生成函数，因此可以把 Generator 赋值给 对象的Symbol.iterator属性，从而使得该对象具有 Iterator 接口。
+    let iterator = {};
+    iterator[Symbol.iterator] = function* () {
+        yield '实例5-1'
+        yield '实例5-2'
+        yield '实例5-3'
+    }
+
+    console.log([...iterator])
+
+    // Generator函数返回遍历器对象,对象本身也具有Symbol.iterator属性，执行后返回自身。
+    {
+        function* gen() {
+
+        }
+
+        var g = gen()
+
+        console.log('实例5 g === g[Symbol.iterator]',g === g[Symbol.iterator]());
+    }
+}
+
+
+/**
+ *  作者:Seven
+ *  时间:2018/8/2 15:27
+ *  Email:csz.seven@gmail.com
+ *  描述:next 方法的参数
+*/
